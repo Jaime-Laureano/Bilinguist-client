e48f5a23467177f92360b17ebf067bea88d8dacf
JasminBower <jasmin.ibower@gmail.com>
2022-04-07 11:31:04 +0200

    basic form completed

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -6,22 +6,38 @@ import {
 	MenuItem,
 	InputLabel,
 	CheckBox,
+	Button,
+	FormGroup,
 } from "@mui/material";
 
 function SignupForm() {
 	return (
 		<div>
-			<FormControl sx={{ m: 1, minWidth: 120 }}>
-				<InputLabel id='demo-simple-select-label'>Who are you?</InputLabel>
-				<Select
-					labelId='demo-simple-select-label'
-					id='demo-simple-select'
-					value='Other'
-					label='Type'>
-					<MenuItem value={"Teacher"}>Teacher</MenuItem>
-					<MenuItem value={"Student"}>Student</MenuItem>
-				</Select>
-			</FormControl>
+			<FormGroup>
+				<TextField id='filled-basic' label='full name' variant='filled' />
+				<TextField id='filled-basic' label='email' variant='filled' />
+				<TextField id='filled-basic' label='password' variant='filled' />
+				<TextField id='filled-basic' label='country' variant='filled' />
+				<TextField id='filled-basic' label='city' variant='filled' />
+				<FormControl sx={{ m: 1, minWidth: 120 }}>
+					<InputLabel id='demo-simple-select-label'>Who are you?</InputLabel>
+					<Select
+						labelId='demo-simple-select-label'
+						id='demo-simple-select'
+						value='Other'
+						label='Type'>
+						<MenuItem value={"Teacher"}>Teacher</MenuItem>
+						<MenuItem value={"Student"}>Student</MenuItem>
+					</Select>
+				</FormControl>
+				<Button
+					variant='contained'
+					onClick={() => {
+						alert("clicked");
+					}}>
+					Submit
+				</Button>
+			</FormGroup>
 		</div>
 	);
 }
