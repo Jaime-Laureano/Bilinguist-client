36146d8ec737cf7aff037eeab8154911522c0edc
JasminBower <jasmin.ibower@gmail.com>
2022-04-07 14:19:39 +0200

    ui compleet, password and email types added

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	TextField,
 	FormControl,
@@ -10,19 +10,90 @@ import {
 	FormGroup,
 } from "@mui/material";
 
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function SignupForm() {
-	const [formState, setFormState] = useState({});
+	const [formState, setFormState] = useState({
+		"full name": "",
+		email: "",
+		password: "",
+		country: "",
+		city: "",
+		"who are you?": "Student",
+	});
+	const navigate = useNavigate();
+
+	useEffect(() => {}, []);
+
+	const handleRegister = async () => {
+		try {
+			const data = await axios.post("/signup", formState);
+			console.log(data);
+			setFormState({});
+			navigate("/login");
+		} catch (err) {
+			console.error(err);
+			console.log(err.response);
+
+			setFormState({
+				...formState,
+				error: err.response.data.errorMessage || err.message,
+			});
+		}
+	};
+
+	const onFormChange = (event) => setFormState(event.target.value);
+
+	const handleSubmit = () => console.log(formState);
 
 	return (
 		<div>
 			<FormGroup>
-				<TextField id='filled-basic' label='full name' variant='filled' />
-				<TextField id='filled-basic' label='email' variant='filled' />
-				<TextField id='filled-basic' label='password' variant='filled' />
-				<TextField id='filled-basic' label='country' variant='filled' />
-				<TextField id='filled-basic' label='city' variant='filled' />
+				<TextField
+					id='filled-basic'
+					label='full name'
+					variant='filled'
+					onChange={onFormChange}
+					value={formState["full name"]}
+					required
+				/>
+				<TextField
+					id='filled-basic'
+					label='email'
+					variant='filled'
+					onChange={onFormChange}
+					value={formState.email}
+					type='email'
+					required
+				/>
+				<TextField
+					id='filled-basic'
+					label='password'
+					variant='filled'
+					onChange={onFormChange}
+					value={formState.password}
+					type='password'
+					required
+				/>
+				<TextField
+					id='filled-basic'
+					label='country'
+					variant='filled'
+					onChange={onFormChange}
+					value={formState.country}
+				/>
+				<TextField
+					id='filled-basic'
+					label='city'
+					variant='filled'
+					onChange={onFormChange}
+					value={formState.city}
+				/>
 				<FormControl sx={{ m: 1, minWidth: 120 }}>
-					<InputLabel id='demo-simple-select-label'>Who are you?</InputLabel>
+					<InputLabel id='demo-simple-select-label' required>
+						Who are you?
+					</InputLabel>
 					<Select
 						labelId='demo-simple-select-label'
 						id='demo-simple-select'
@@ -32,11 +103,7 @@ function SignupForm() {
 						<MenuItem value={"Student"}>Student</MenuItem>
 					</Select>
 				</FormControl>
-				<Button
-					variant='contained'
-					onClick={() => {
-						alert("clicked");
-					}}>
+				<Button type='submit' variant='contained' onClick={handleSubmit}>
 					Submit
 				</Button>
 			</FormGroup>
