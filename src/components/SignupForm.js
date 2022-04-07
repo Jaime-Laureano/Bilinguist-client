180c90c4a833f4dba77cf2f88258736d75b69cf3
JasminBower <jasmin.ibower@gmail.com>
2022-04-07 15:53:38 +0200

    input errors solved

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -15,38 +15,20 @@ import { useNavigate } from "react-router-dom";
 
 function SignupForm() {
 	const [formState, setFormState] = useState({
-		"full name": "",
+		fullName: "",
 		email: "",
 		password: "",
 		country: "",
 		city: "",
-		"who are you?": "Student",
 	});
 	const navigate = useNavigate();
 
 	useEffect(() => {}, []);
 
-	const handleRegister = async () => {
-		try {
-			const data = await axios.post("/signup", formState);
-			console.log(data);
-			setFormState({});
-			navigate("/login");
-		} catch (err) {
-			console.error(err);
-			console.log(err.response);
-
-			setFormState({
-				...formState,
-				error: err.response.data.errorMessage || err.message,
-			});
-		}
-	};
-
 	const onFormChange = (event) =>
 		setFormState({ ...formState, [event.target.name]: event.target.value });
 
-	const handleSubmit = () => console.log(formState, "<<<<<,,");
+	const handleSubmit = () => console.log(formState);
 
 	return (
 		<div>
@@ -55,14 +37,16 @@ function SignupForm() {
 					id='filled-basic'
 					label='full name'
 					variant='filled'
+					name='fullName'
 					onChange={onFormChange}
-					value={formState["full name"]}
+					value={formState.fullName}
 					required
 				/>
 				<TextField
 					id='filled-basic'
 					label='email'
 					variant='filled'
+					name='email'
 					onChange={onFormChange}
 					value={formState.email}
 					type='email'
@@ -72,6 +56,7 @@ function SignupForm() {
 					id='filled-basic'
 					label='password'
 					variant='filled'
+					name='password'
 					onChange={onFormChange}
 					value={formState.password}
 					type='password'
@@ -81,6 +66,7 @@ function SignupForm() {
 					id='filled-basic'
 					label='country'
 					variant='filled'
+					name='country'
 					onChange={onFormChange}
 					value={formState.country}
 				/>
@@ -88,22 +74,11 @@ function SignupForm() {
 					id='filled-basic'
 					label='city'
 					variant='filled'
+					name='city'
 					onChange={onFormChange}
 					value={formState.city}
 				/>
-				<FormControl sx={{ m: 1, minWidth: 120 }}>
-					<InputLabel id='demo-simple-select-label' required>
-						Who are you?
-					</InputLabel>
-					<Select
-						labelId='demo-simple-select-label'
-						id='demo-simple-select'
-						value='Other'
-						label='Type'>
-						<MenuItem value={"Teacher"}>Teacher</MenuItem>
-						<MenuItem value={"Student"}>Student</MenuItem>
-					</Select>
-				</FormControl>
+
 				<Button type='submit' variant='contained' onClick={handleSubmit}>
 					Submit
 				</Button>
