29477bc22299c27918c75cacdef4c612424b0a55
JasminBower <jasmin.ibower@gmail.com>
2022-04-08 15:40:19 +0200

    added teachers search form and add teaxher component

---
+++ src/pages/FindTeacher.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AddTeacherForm from "../components/AddTeacherForm";
+
+function FindTeacher() {
+	return (
+		<div>
+			<h1>list of all teachers here</h1>
+			<AddTeacherForm />
+		</div>
+	);
+}
+
+export default FindTeacher;
