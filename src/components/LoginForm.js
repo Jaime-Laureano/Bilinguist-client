acda68e60d99a42a4caa1299d161c18b68cee6ba
JasminBower <jasmin.ibower@gmail.com>
2022-04-08 10:36:30 +0200

    login page fixed

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,46 @@
-import { FormGroup, TextField } from "@mui/material";
-import React, { useState } from "react";
+import { FormGroup, TextField, Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
-	const [loginFormState, setLoginFormState] = useState({});
+	const [loginFormState, setLoginFormState] = useState({
+		email: "",
+		password: "",
+	});
+	const navigate = useNavigate();
+	useEffect(() => {}, []);
 
 	const onFormChange = (event) =>
 		setLoginFormState({
 			...loginFormState,
 			[event.target.name]: event.target.value,
 		});
+
+	const handleSubmit = async () => {
+		console.log(loginFormState, "*********");
+		try {
+			const data = await axios.post(
+				"http://localhost:5005/api/login",
+				loginFormState,
+				{ withCredentials: true },
+			);
+			console.log(data.data, "Brummie login data");
+			setLoginFormState({});
+			navigate("/profile");
+		} catch (err) {
+			console.error(err, "<<<<<");
+			// console.log(err.response);
+
+			// setFormState({
+			// 	...formState,
+			// 	error: err.response.data.errorMessage || err.message,
+			// });
+		}
+	};
 	return (
 		<div>
 			<FormGroup>
-				<TextField
-					id='filled-basic'
-					label='full name'
-					variant='filled'
-					name='password'
-					onChange={onFormChange}
-					value={loginFormState.password}
-					required
-				/>
-
 				<TextField
 					id='filled-basic'
 					label='email'
@@ -32,6 +51,18 @@ function LoginForm() {
 					type='email'
 					required
 				/>
+				<TextField
+					id='filled-basic'
+					label='password'
+					variant='filled'
+					name='password'
+					onChange={onFormChange}
+					value={loginFormState.password}
+					required
+				/>
+				<Button type='submit' variant='contained' onClick={handleSubmit}>
+					Submit
+				</Button>
 			</FormGroup>
 		</div>
 	);
