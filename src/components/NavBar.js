0a73b8a2813a661c6c63ffabab1a8d67acf69ebe
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2022-04-14 09:43:52 +0100

    Started CSS, added footer and error components. Working on bugs.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
 import { Link } from "react-router-dom";
+import home from "../logos/home.png"
 
 function NavBar() {
 	return (
-		<div>
-			<Link to='/practice'>practice here</Link>
-			<Link to='/message-board'>chat here</Link>
-			<Link to='/student-profile'>🏠</Link>
-			<Link to='/find-teacher'>Find a teacher</Link>
-			<Link to='/video-chat'>call here</Link>
-			<Link to='/logout'>Logout</Link>
+		<div className="nav">
+			<Link to='/student-profile'>
+			<img src={home} alt="home icon" height={50} />
+			</Link>
+			<Link to='/practice'>PRACTICE</Link>
+			<Link to='/message-board'>CHAT HERE</Link>
+			<Link to='/find-teacher'>FIND A TEACHER</Link>
+			<Link to='/video-chat'>CALL</Link>
+			<Link to='/logout'>LOGOUT</Link>
 		</div>
 	);
 }
